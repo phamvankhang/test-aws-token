3fe2216571b9bd99b4b51bb2c7ef7f79de5d728f
khangpv <vkphambn@gmail.com>
2017-06-09 20:24:17 +0700

    init

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const bodyParser  = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
     // var bodyString = JSON.stringify(req.body)
     console.log(req.body)
     res.send('hello')
 })
-app.post('/', function (req, res) {
+app.post('*', function (req, res) {
     // var bodyString = JSON.stringify(req.body)
     console.log('------------ DU LIEU GUI LEN ------------')
     console.log(req.body)
